1908b4ecfdbf0c2cfd466ba761a5cb743160af86
kr616m <kiran.r@xandr.com>
2022-10-10 03:26:16 +0530

    commit for pipeline'

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM openjdk:8
 ADD target/my-maven-docker-project.jar my-maven-docker-project.jar
 ENTRYPOINT ["java", "-jar","my-maven-docker-project.jar"]
-EXPOSE 8080
--- src/main/java/com/sl/assignment1/Start.java
+++ src/main/java/com/sl/assignment1/Start.java
@@ -3,5 +3,7 @@ package com.sl.assignment1;
 public class Start {
     public static void main(String a[]) {
         System.out.println("<==============My demo project for the pipeline assignment=============>");
+        System.out.println("<==============My demo project for the pipeline assignment=============>");
+        System.out.println("<==============My demo project for the pipeline assignment=============>");
     }
 }
